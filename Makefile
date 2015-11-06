de3fa57a2e009eec5072a6f2c01caec4854f3b6e
gpradeepufl <gpradeepufl@users.noreply.github.com>
2015-11-05 22:30:02 -0500

    Some what good shape

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 all:
 	g++ -std=c++11 -o ss smain.cpp Socket.cpp
 	g++ -std=c++11 -o sc cmain.cpp Socket.cpp -lpthread
+	cp sc c1/
+	cp sc c2/
+	cp sc c3/
 s:
 	g++ -std=c++11 -o ss smain.cpp Socket.cpp
 c:
--- cmain.cpp
+++ cmain.cpp
@@ -16,7 +16,15 @@ struct common_data {
 	vector <bool> is_fcpresent;
 	int nChunksNeeded;
 };
-
+void make_final_file(struct common_data *cd) {
+	FILE *outfp = fopen((cd->ofname).c_str(), "w" );
+	for (int i=0; i < cd->fchunks.size(); i++) {
+		fwrite (cd->fchunks[i], sizeof(char), MAXRECV, outfp);
+		delete cd->fchunks[i];
+	}
+	fclose(outfp);
+	return;
+}
 void *uploader(void *cdata) {
 	char ctrl[MAXRECV];
 	vector <int> sent;
@@ -32,18 +40,29 @@ void *uploader(void *cdata) {
 		serverSkt->accept(*bind_sock);
 		std::cout << "[UPLOADER]: Waiting to connect to Client\n";
 		bind_sock->recv(ctrl);
+		bind_sock->send(to_string(1));
 		int num_tosend = atoi(ctrl);
+		std::cout << "[UPLOADER]: Client Connected: " <<num_tosend<<"\n";
+
 		try {
 			while (sent.size() != num_tosend) {
 				memset (ctrl, 0, MAXRECV + 1);
 				bind_sock->recv(ctrl);
 				int cid = atoi(ctrl);
+				cout << "[UPLOADER]: Client requested CID: " << cid;
+				bind_sock->send(to_string(1));
+
 				if (cd->is_fcpresent[cid]) {
-					bind_sock->send(to_string(1));
+					bind_sock->send(to_string(10));
+					bind_sock->recv(ctrl);
 					bind_sock->send(cd->fchunks[cid], MAXRECV);
+					bind_sock->recv(ctrl);
 					sent.push_back(cid);
+					cout << "TRANSFERED \n";
 				} else {
-					bind_sock->send(to_string(0));
+					bind_sock->send(to_string(11));
+					bind_sock->recv(ctrl);
+					cout << "NOT PRESENT YET \n";
 				}
 			}
 		} catch ( SocketException& ) {}
@@ -66,31 +85,44 @@ void *downloader(void *cdata) {
 		try
 		{
 			Socket csocket ( false, "localhost", cd->dwport);
+			std::cout << "[DOWNLOADER]: Connected to SERVER\n" ;
 			csocket.send(to_string(cd->nChunksNeeded));
+			memset(ctrl, 0, MAXRECV + 1);
+			csocket.recv(ctrl); // ACK
 			int cid = 0;
 			while (recvd.size() != cd->nChunksNeeded) {
 				if (cid == n)
 					cid = 0;
 				if (cd->is_fcpresent[cid]) {cid++; continue;}
 
-				csocket.send(to_string(cid));
+				csocket.send(to_string(cid)); // Send CID
 				memset(ctrl, 0, MAXRECV + 1);
-				csocket.recv(ctrl);
-				if (!atoi(ctrl)) {cid++; continue;}
-
+				csocket.recv(ctrl);// RECV ACK
+				memset(ctrl, 0, MAXRECV + 1);
+				csocket.recv(ctrl);// RECV IF_PRESENT
+				csocket.send(to_string(1));
+				if (atoi(ctrl) == 11) {cid++; continue;}
+				// PRESENT - RECV DATA AND SEND ACK
 				csocket.recv(cd->fchunks[cid]);
+				csocket.send(to_string(1));
+				cd->is_fcpresent[cid] = true;
+				recvd.push_back(cid);
 				cid++;
 			}
+			cout << "[DOWNLOADER]: DONE with DOWNLOADING\n";
+			make_final_file(cd);
+			pthread_exit(NULL);
 			break;
 		} catch ( SocketException& e ) {
 			cout << "[DOWNLOADER] waiting for uploader to join"<< "\n";
-			sleep(5);
+			sleep(2);
 		} 
 	}
+	make_final_file(cd);
 	pthread_exit(NULL);
 }
 
-void build_fchunks(vector<char *> &fcs, vector <bool> is_fc, int n) {
+void build_fchunks(vector<char *> &fcs, vector <bool> &is_fc, int n) {
 	for (int i=0; i<n; i++) {
 		char *newChunk = new char[MAXRECV];
 		memset(newChunk, 0, MAXRECV+1);
@@ -125,23 +157,31 @@ int main ()
 		char buf[MAXRECV + 1];
 		string s;
 		client_socket.recv(buf);
+		client_socket.send(to_string(1));
 		int nchunks = atoi(buf);
 		build_fchunks(cdata.fchunks, cdata.is_fcpresent, nchunks);
+
         	memset ( buf, 0, MAXRECV + 1 );		
 		client_socket.recv(buf);
+		client_socket.send(to_string(1));
 		cdata.ofname = buf;
 		cout<<"File Name: " << cdata.ofname << " Chunk size: "<<nchunks<<endl;
+
         	memset ( buf, 0, MAXRECV + 1 );		
 		client_socket.recv(buf);
+		client_socket.send(to_string(1));
 		int num_chunks_iget = atoi(buf);
 		cdata.nChunksNeeded = nchunks- num_chunks_iget;
+		
 		for (int i =0; i < num_chunks_iget; i++) {
 			memset ( buf, 0, MAXRECV + 1 );		
 			client_socket.recv(buf);
+			client_socket.send(to_string(1));
 			int cid = atoi(buf);
 			int rd = client_socket.recv(cdata.fchunks[cid]);
+			client_socket.send(to_string(1));
+			cout << "Received CHUNK# "<<cid << " "<< rd <<" Bytes"<<endl;
 			cdata.is_fcpresent[cid] = true;
-			cout << "Received CHUNK# "<<i << " "<< rd <<" Bytes"<<endl;
 		}
 	} catch ( SocketException& e ) {
 		cout << "Exception was caught:" << e.description() << "\n";
@@ -157,5 +197,7 @@ int main ()
 		cout << "Error:unable to create downloader thread," << rc << endl;
          	exit(-1);
       	}
+	pthread_join(up_th, NULL);
+	pthread_join(dw_th, NULL);
    	pthread_exit(NULL);
 }
